20a021f9bd618798784b4b2735faeb1616280533
Breno Marques <breno.marques@hbsis.com.br>
2020-08-21 11:14:15 -0300

    Creating the products provider

---
+++ ECommerce.Api.Products/Interfaces/IProductsProvider.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommerce.Api.Products.Models;
+
+namespace ECommerce.Api.Products.Interfaces
+{
+    public interface IProductsProvider
+    {
+        Task<(bool IsSuccess, IEnumerable<Product> Product, string ErrorMessage)> GetProductsAsync();
+    }
+}
---
+++ ECommerce.Api.Products/Profiles/ProductProfile.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ECommerce.Api.Products.Profiles
+{
+    public class ProductProfile: AutoMapper.Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Db.Product, Models.Product>();
+            
+        }
+    }
+}
